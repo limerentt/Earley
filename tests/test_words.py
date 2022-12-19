3df87cf994032fbaff4bb1bded691335edaaab25
limerent <polev.am@phystech.edu>
2022-12-20 01:16:54 +0300

    added complex tests

--- tests/test_words.py
+++ tests/test_words.py
@@ -1,4 +1,5 @@
 import src.grammar as grammar
+import pytest
 
 def test_word():
     GR = grammar.Grammar("./tests/test_homskiy.txt")
@@ -15,4 +16,9 @@ def test_empty_word():
 def test_no_empty_word():
     GR = grammar.Grammar("./tests/test_no_eps.txt")
     assert not GR.word_is_suitable("$$$")
+    
+@pytest.mark.parametrize(("word", "answer"), [("aabaa", False), ("ababbbaba", True), ("aabbbabbabab", True)])
+def test_huge(word, answer):
+    GR = grammar.Grammar("./tests/test_huge.txt")
+    assert GR.word_is_suitable(word) == answer
     
